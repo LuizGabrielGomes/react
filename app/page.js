884beb3c3849cf20582661247009c73646a85168
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-08-26 23:50:07 +0100

    form useState

--- app/page.js
+++ app/page.js
@@ -1,45 +1,32 @@
 "use client"
 
+import { useState } from "react"
+
 export default function Home() {
+  const [name, setName] = useState("")
+  const [surname, setSurname] = useState("")
 
+  const handleChangeName = (event) => {
+    console.log(event.target.value)
+    setName(event.target.value)
+  }
 
-const students = [
-  { name: 'Quincy', grade: 96 },
-  { name: 'Jason', grade: 84 },
-  { name: 'Alexis', grade: 100 },
-  { name: 'Sam', grade: 65 },
-  { name: 'Katie', grade: 90 }
-];
-
-// console.log(students)
-// console.log(students[0])
-// console.log(students[0].name)
-// console.log(students[0].grade)
-// // Dado o array de objetos acima, faça uma função que retorne somente os nomes de cada estudante.
-
-// function pick(students) {
-//   return students.map(item => item.name)
-// }
-
-// console.log(pick(students))
-
-// // faça uma função que retorne apenas os estudantes com nota maior ou igual a 90.
-
-
-
-// function take(students) {
-//     return students.map(item => item.grade >=90 ? item.name : "nok")
-// }
-// console.log(take(students))
-
-
-
-
-
+  const handleChangeSurname = (event) => {
+    console.log(event.target.value)
+    setSurname(event.target.value)
+  }
 
   return (
-    <ul>
-      {students.map(item => item.grade >=90 ? <li key={item.name}>{item.name}</li> : null)}
-    </ul> 
+    <>
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" value={name} onChange={handleChangeName} />   
+      <br/>
+      <br/>
+      <label htmlFor="surname">Surname</label>
+      <input type="text" id="surname" value={surname} onChange={handleChangeSurname} />
+      <br/>
+      <br/>
+      <button>click</button> 
+    </>
   )
 }
