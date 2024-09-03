2a99fd27080acb232e21c5eb03d58de4f9f93631
Luiz Gabriel Gomes <luizgabrielgomes9686@gmail.com>
2024-09-03 23:49:53 +0100

    update state

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import { useState } from "react"
 
 export default function Home() {
-  const [name, setName] = useState("")
+  const [name, setName] = useState("")  //  o name e o "state" e o setName e o "setState". e ambos sao funcoes do react para mudar o "state", ou seja, para mudar o estado visual da aplicacao "DOM" 
   const [surname, setSurname] = useState("")
 
   const handleChangeName = (event) => {
@@ -18,8 +18,14 @@ export default function Home() {
 
   return (
     <>
+      <p>{name}</p>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" value={name} onChange={handleChangeName} />   
+      <input 
+        type="text" 
+        id="name" 
+        value={name}   // no value estamos passando o state name para pegar o valor inicial de value  
+        onChange={handleChangeName}    // no onChange estamos passando uma funcao handleChangeName e dentro dessa funcao estamos passando setName, sendo que o setName e responsalvel por "atualizar" o state    
+      />   
       <br/>
       <br/>
       <label htmlFor="surname">Surname</label>
